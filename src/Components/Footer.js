cb4bb6b2e11a02ed46766886469625a7c63371e5
un-earthly <vijayalamin@gmail.com>
2022-05-21 02:20:17 -0700

    footer updated

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -9,24 +9,43 @@ export default function Footer() {
                     <p>New DOHS, Mohakhali, Dhaka, Bangladesh</p></div>
             </div>
             <div>
-                <span class="footer-title">Company</span>
-                <a class="link link-hover">Branding</a>
-                <a class="link link-hover">Design</a>
-                <a class="link link-hover">Marketing</a>
-                <a class="link link-hover">Advertisement</a>
+                <span class="text-white font-bold uppercase">Company</span>
+                <a class="link link-hover">About
+                </a>
+                <a class="link link-hover">Project
+                </a>
+                <a class="link link-hover"> Our Team
+                </a>
+                <a class="link link-hover">Terms Conditions
+                </a>
+                <a class="link link-hover">Submit Listing
+                </a>
             </div>
             <div>
-                <span class="footer-title">Quick Links</span>
-                <a class="link link-hover">About us</a>
-                <a class="link link-hover">Contact</a>
-                <a class="link link-hover">Jobs</a>
-                <a class="link link-hover">Press kit</a>
+                <span class="text-white font-bold uppercase">Quick Links</span>
+                <a class="link link-hover">Quick Links
+                    us</a>
+                <a class="link link-hover">Rentals
+                </a>
+                <a class="link link-hover"> Sales
+                </a>
+                <a class="link link-hover">Contact
+                </a>
+                <a class="link link-hover">Our blog
+                </a>
             </div>
-            <div>
-                <span class="footer-title">About us</span>
-                <a class="link link-hover">Terms of use</a>
-                <a class="link link-hover">Privacy policy</a>
-                <a class="link link-hover">Cookie policy</a>
+            <div class="w-2/3">
+                <span class="text-white font-bold uppercase">About us</span>
+                <p>Lorem ipsum dolor sit amet, consectetur
+                    adipiscing elit. Purus commodo ipsum
+                    duis laoreet maecenas. Feugiat </p>
+                <div class="text-2xl space-x-4 flex items-center justify-center">
+
+                    <a class="link link-hover"><i className="bi bi-facebook"></i></a>
+                    <a class="link link-hover"><i className="bi bi-instagram"></i></a>
+                    <a class="link link-hover"><i className="bi bi-linkedin"></i></a>
+                    <a class="link link-hover"><i className="bi bi-youtube"></i></a>
+                </div>
             </div>
         </footer>
     )
